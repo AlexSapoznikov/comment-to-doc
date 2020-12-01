d339dba83ffc199605f3c56aaf73e52162beb937
Alex Sapoznikov <alex@nodeswat.com>
2020-12-01 11:15:44 +0200

    Add multiline comment parse tests

--- tests/comment-style.parse.test.ts
+++ tests/comment-style.parse.test.ts
@@ -12,7 +12,7 @@ const testCases = [
        * Content
        */
     `,
-    out: {
+    out: [{
       "tag": "Tag",
       "alias": "alias",
       "type": "type",
@@ -22,13 +22,13 @@ const testCases = [
       ],
       "description": "description",
       "content": "Content"
-    }
+    }]
   },
   {
     in: `
       /** @Tag:alias {type} [extra,data] description */
     `,
-    out: {
+    out: [{
       "tag": "Tag",
       "alias": "alias",
       "type": "type",
@@ -38,13 +38,13 @@ const testCases = [
       ],
       "description": "description",
       "content": ""
-    }
+    }]
   },
   {
     in: `
       /* @Tag:alias {type} [extra,data] description */
     `,
-    out: {
+    out: [{
       "tag": "Tag",
       "alias": "alias",
       "type": "type",
@@ -54,7 +54,7 @@ const testCases = [
       ],
       "description": "description",
       "content": ""
-    }
+    }]
   },
   {
     in: `
@@ -62,7 +62,7 @@ const testCases = [
       Content
       */
     `,
-    out: {
+    out: [{
       "tag": "Tag",
       "alias": "alias",
       "type": "type",
@@ -72,7 +72,7 @@ const testCases = [
       ],
       "description": "description",
       "content": "Content"
-    }
+    }]
   },
   {
     in: `
@@ -80,7 +80,7 @@ const testCases = [
        * Content
        */
     `,
-    out: {
+    out: [{
       "tag": "Tag",
       "alias": "alias",
       "type": "type",
@@ -90,14 +90,75 @@ const testCases = [
       ],
       "description": "description",
       "content": "Content"
-    }
+    }]
+  },
+  {
+    in: `
+      /** 
+       * @Tag1:alias {type} [extra,data] description
+       * Content
+       *
+       * @Tag2:alias2
+       *
+       * @Tag3
+       *
+       * @Tag4 tag4 description
+       * @Tag5
+       * tag 5 content
+       */
+    `,
+    out: [
+      {
+        "tag": "Tag1",
+        "alias": "alias",
+        "type": "type",
+        "extras": [
+          "extra",
+          "data"
+        ],
+        "description": "description",
+        "content": "Content"
+      },
+      {
+        "tag": "Tag2",
+        "alias": "alias2",
+        "type": "",
+        "extras": [],
+        "description": "",
+        "content": ""
+      },
+      {
+        "tag": "Tag3",
+        "alias": "",
+        "type": "",
+        "extras": [],
+        "description": "",
+        "content": ""
+      },
+      {
+        "tag": "Tag4",
+        "alias": "",
+        "type": "",
+        "extras": [],
+        "description": "tag4 description",
+        "content": ""
+      },
+      {
+        "tag": "Tag5",
+        "alias": "",
+        "type": "",
+        "extras": [],
+        "description": "",
+        "content": "tag 5 content"
+      }
+    ]
   },
 ]
 
 describe('Parse tests for different comment styles', () => {
   testCases.forEach(testCase => {
     it (testCase.in, () => {
-      const [parsed] = testParser(testCase.in);
+      const parsed = testParser(testCase.in);
       console.log(JSON.stringify(parsed, null, 2))
       expect(parsed).toMatchObject(testCase.out);
     });
